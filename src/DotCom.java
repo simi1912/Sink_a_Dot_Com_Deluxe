d66936663062064171def23ddd10605e3232791d
Simi <nioasca@gmail.com>
2018-08-30 22:56:00 +0300

    Initial commit

---
+++ src/DotCom.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+public class DotCom {
+
+    private  ArrayList<String> locationCells;
+
+    public void setLocationCells(ArrayList<String> loc) {
+        locationCells = loc;
+    }
+
+    public String checkYourself(String userImput) {
+
+        String result = "miss";
+
+        int index = locationCells.indexOf(userImput);
+
+        if(index>=0) {
+
+            locationCells.remove(index);
+
+            if (locationCells.isEmpty()){
+
+                result="kill";
+            }else {
+                result = "hit";
+            }
+        }
+
+        return result;
+
+    }
+}
